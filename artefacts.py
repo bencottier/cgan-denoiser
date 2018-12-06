fec4f1f2f4d9ff437895c451d99868af909f96c4
Ben Cottier <bcottier2@gmail.com>
2018-12-06 17:10:11 +1000

    Move test code to main scope

--- artefacts.py
+++ artefacts.py
@@ -15,24 +15,25 @@ def add_gaussian_noise(data, stdev=0.1, mean=0.0):
     return normalise(data, (0, 1)) + np.random.normal(mean, stdev, data.shape)
 
 
-# Test
-from scipy.misc import ascent
-import tensorflow as tf
-import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    # Test
+    from scipy.misc import ascent
+    import tensorflow as tf
+    import matplotlib.pyplot as plt
 
-(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 
-# data = ascent()
-data = train_images
-print("Data shape: ", data.shape)
-data = add_gaussian_noise(data, 0.2)
-# plt.imshow(data[0], cmap='gray')
+    # data = ascent()
+    data = train_images
+    print("Data shape: ", data.shape)
+    data = add_gaussian_noise(data, 0.2)
+    # plt.imshow(data[0], cmap='gray')
 
-fig = plt.figure(figsize=(4,4))
+    fig = plt.figure(figsize=(4,4))
 
-for i in range(16):
-    plt.subplot(4, 4, i+1)
-    plt.imshow(data[i, :, :], cmap='gray')
-    plt.axis('off')
+    for i in range(16):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(data[i, :, :], cmap='gray')
+        plt.axis('off')
 
-plt.show()
+    plt.show()
