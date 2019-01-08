13ed33a267da93964d7ddced3080bb1cdd157876
Ben Cottier <bcottier2@gmail.com>
2019-01-08 16:23:18 +1000

    Update experiment for on-the-fly processing

--- test_undersampled.py
+++ test_undersampled.py
@@ -9,6 +9,7 @@ Methods include compressed sensing and chaotic sensing.
 author: Ben Cottier (git: bencottier)
 """
 from __future__ import absolute_import, division, print_function
+import data
 import artefacts
 import data_processing
 from config import Config as config
@@ -50,7 +51,7 @@ def discriminator_loss(real_output, generated_output):
     return total_loss
 
 
-def train_step(inputs, labels):
+def train_step(labels, inputs):
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         generated_images = generator(inputs, training=True)
 
@@ -80,7 +81,7 @@ def train_step(inputs, labels):
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.variables))
 
 
-def validate_step(inputs, labels):
+def validate_step(labels, inputs):
     generated_images = generator(inputs, training=False)
 
     real_output = discriminator(labels, training=False)
@@ -107,16 +108,18 @@ def train(train_dataset, valid_dataset=None, epochs=1):
     for epoch in range(epochs):
         start = time.time()
 
-        for x, y in train_dataset.make_one_shot_iterator():
-            train_step(x, y)
+        for y, x in train_dataset:
+            y, x = data_processing.preprocess_train_batch(y, x)
+            train_step(y, x)
 
-        for x, y in valid_dataset.make_one_shot_iterator():
-            validate_step(x, y)
+        for y, x in valid_dataset:
+            y, x = data_processing.preprocess_train_batch(y, x)
+            validate_step(y, x)
 
-        generate_and_save_images(generator,
-                                 epoch + 1,
-                                 selected_inputs,
-                                 selected_labels)
+        # generate_and_save_images(generator,
+        #                          epoch + 1,
+        #                          selected_labels,
+        #                          selected_inputs)
         
         # saving (checkpoint) the model every few epochs
         if (epoch + 1) % config.save_per_epoch == 0:
@@ -125,23 +128,24 @@ def train(train_dataset, valid_dataset=None, epochs=1):
         print ('Time taken for epoch {} is {} sec'.format(epoch + 1,
                                                           time.time()-start))
     # generating after the final epoch
-    generate_and_save_images(generator,
-                             epochs,
-                             selected_inputs,
-                             selected_labels)
+    # generate_and_save_images(generator,
+    #                          epochs,
+    #                          selected_labels,
+    #                          selected_inputs)
 
 
-def test(model, inputs, labels):
+def test(model, dataset):
     print("Running model on test set...")
-    prediction_time = np.zeros(inputs.shape[0], dtype=float)
-    rmse = np.zeros(inputs.shape[0], dtype=np.float64)
-    psnr = np.zeros(inputs.shape[0], dtype=np.float64)
-    for i in range(inputs.shape[0]):
+    prediction_time = np.zeros(config.n_test, dtype=float)
+    rmse = np.zeros(config.n_test, dtype=np.float64)
+    psnr = np.zeros(config.n_test, dtype=np.float64)
+    for i, (y, x, _) in enumerate(dataset):
+        y, x = data_processing.preprocess_train_batch(y, x)
         t0 = time.time()
-        prediction = generator(inputs[i:i+1], training=False)
+        prediction = generator(x, training=False)
         prediction_time[i] = time.time() - t0
-        rmse[i] = data_processing.rmse(labels[i:i+1], prediction, norm=2)
-        psnr[i] = data_processing.psnr(labels[i:i+1], prediction, max_diff=1)
+        rmse[i] = data_processing.rmse(y, prediction, norm=2)
+        psnr[i] = data_processing.psnr(y, prediction, max_diff=1)
     print("Results over test set:")
     print("Time mean: {:.4f} sec".format(np.mean(prediction_time)))
     print("Time stdv: {:.4f} sec".format(math.sqrt(np.var(prediction_time))))
@@ -151,7 +155,7 @@ def test(model, inputs, labels):
     print("PSNR stdv: {:.4f} dB".format(math.sqrt(np.var(psnr))))
 
 
-def generate_and_save_images(model, epoch, test_inputs, test_labels):
+def generate_and_save_images(model, epoch, test_labels, test_inputs):
     if model is None:
         predictions = test_inputs
     else:
@@ -190,7 +194,7 @@ def generate_and_save_images(model, epoch, test_inputs, test_labels):
     # plt.show()  # TODO
 
 
-def plot_samples(model, inputs, labels, n_samples):
+def plot_samples(model, labels, inputs, n_samples):
     """
     Plot samples of a dataset with comparison and error.
     """
@@ -211,8 +215,7 @@ def plot_samples(model, inputs, labels, n_samples):
         plt.title('Ground truth')
         plt.colorbar()
         plt.subplot(2, 2, 4)
-        plt.imshow(labels[i, :, :, 0], vmin=-1, vmax=1, cmap='gray')
-        plt.imshow(np.abs(labels[i, :, :, 0] - prediction[i, :, :, 0]), cmap='jet', alpha=0.3)
+        plt.imshow(np.abs(labels[i, :, :, 0] - prediction[i, :, :, 0]), cmap='jet')
         plt.title('Abs. error overlay')
         plt.colorbar()
         plt.show()
@@ -224,7 +227,7 @@ def log_metric(value, name):
 
 
 if __name__ == '__main__':
-    # model_path = "out/fractal_oasis1_cgan/model/2018-12-17-16-20-36"
+    # Load a trained model
     # model_path = "out/fractal_oasis1_cgan_no_disc/model/2018-12-20-12-39-54"
 
     # Make directories for this run
@@ -241,30 +244,29 @@ if __name__ == '__main__':
     global_step = tf.train.get_or_create_global_step()
 
     # Load the data
-    # Train and test
-    (train_inputs, train_labels), (test_inputs, test_labels), case_list = data_processing.get_oasis_dataset(
-        config.input_path, config.label_path, 
-        config.test_cases, config.max_training_cases, config.train_size)
-    # Test only
-    # test_inputs, test_labels = data_processing.get_oasis_dataset_test(
-    #     config.input_path, config.label_path, 
-    #     config.test_cases, len(config.test_cases), config.train_size)
-
-    # Training set
-    full_dataset = tf.data.Dataset.from_tensor_slices((train_inputs, train_labels))
-    full_dataset.shuffle(config.buffer_size)
-    valid_size = int(config.validation_split * train_inputs.shape[0])
-    train_dataset = full_dataset.skip(valid_size).batch(config.batch_size)
-    valid_dataset = full_dataset.take(valid_size).batch(config.batch_size)
+    dataset = data.load_data()
+    train_dataset = dataset["train"]()
+    valid_dataset = dataset["valid"]()
+    test_dataset = dataset["test"]()
 
     # Test set
     # Set up some random (but consistent) test cases to monitor
-    n_test_samples = 16
-    random_indices = np.random.choice(np.arange(test_inputs.shape[0]),
-                                      n_test_samples,
-                                      replace=False)
-    selected_inputs = test_inputs[random_indices]
-    selected_labels = test_labels[random_indices]
+    # n_test_samples = 16
+    # random_indices = np.random.choice(np.arange(config.n_test), 
+    #                                   n_test_samples,
+    #                                   replace=False)
+    # selected_labels = np.zeros((n_test_samples, config.raw_size, config.raw_size, config.channels),
+    #                            dtype=np.float32)
+    # selected_inputs = np.zeros((n_test_samples, config.raw_size, config.raw_size, config.channels),
+    #                            dtype=np.float32)
+    # count = 0
+    # for i, (y, x, _) in enumerate(test_dataset):
+    #     if i >= n_test_samples:
+    #         break
+    #     y, x = data_processing.preprocess_train_batch(y, x)
+    #     selected_labels[count] = y[0]
+    #     selected_inputs[count] = x[0]
+    #     count += 1
     
     # Set up the models for training
     generator = model.make_generator_model()
@@ -281,7 +283,7 @@ if __name__ == '__main__':
     # checkpoint = tf.train.Checkpoint(optimizer=generator_optimizer, model=generator)
 
     # Train
-    generate_and_save_images(None, 0, selected_inputs, selected_labels)  # baseline
+    # generate_and_save_images(None, 0, selected_labels, selected_inputs)  # baseline
     print("\nTraining...\n")
     # Compile training function into a callable TensorFlow graph (speeds up execution)
     train_step = tf.contrib.eager.defun(train_step)
@@ -290,8 +292,8 @@ if __name__ == '__main__':
 
     # Test
     checkpoint.restore(tf.train.latest_checkpoint(model_path))
-    test(generator, test_inputs, test_labels)
-    # plot_samples(generator, selected_inputs, selected_labels, n_test_samples)
-    # generate_and_save_images(generator, config.max_epoch + 1, selected_inputs, selected_labels)
+    test(generator, test_dataset)
+    # plot_samples(generator, selected_labels, selected_inputs, n_test_samples)
+    # generate_and_save_images(generator, config.max_epoch + 1, selected_labels, selected_inputs)
 
     print("End of main program")
