6cac219cadd2bf5e02253c3c03ad625b2d958740
Ben Cottier <bcottier2@gmail.com>
2019-01-08 12:48:44 +1000

    Adjust data loading for on-the-fly processing

--- data.py
+++ data.py
@@ -6,19 +6,28 @@ Methods for retrieving data at run time.
 author: Ben Cottier (git: bencottier)
 """
 from config import Config as config
+import artefacts
 from utils import imread
+import nibabel as nib
 import os
 
 
-def load(path):
+def load_pair(path):
     for i in os.listdir(path):
         pair = imread(os.path.join(path, i))
         raw_label, raw_input = pair[:, :config.raw_size], pair[:, config.raw_size:]
         yield (raw_label, raw_input, i)
 
 
+def load(path):
+    for i in os.listdir(path):
+        raw_label = nib.load(os.path.join(path, i)).get_data()
+        raw_input = artefacts.add_turbulence(raw_label)
+        yield (raw_label, raw_input, i)
+
+
 def load_data():
     data = dict()
     data["train"] = lambda: load(config.data_path + "/train")
-    data["test"] = lambda: load(config.data_path + "/test")
+    data["valid"] = lambda: load(config.data_path + "/valid")
     return data
