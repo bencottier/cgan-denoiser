e30cfb100305615cd745b2ccbcf3bd3481136549
Ben Cottier <bcottier2@gmail.com>
2019-01-08 14:40:41 +1000

    Add test set and extra axes to data

--- data.py
+++ data.py
@@ -9,6 +9,7 @@ from config import Config as config
 import artefacts
 from utils import imread
 import nibabel as nib
+import numpy as np
 import os
 
 
@@ -23,11 +24,14 @@ def load(path):
     for i in os.listdir(path):
         raw_label = nib.load(os.path.join(path, i)).get_data()
         raw_input = artefacts.add_turbulence(raw_label)
+        raw_label = raw_label[np.newaxis, ..., np.newaxis]
+        raw_input = raw_input[np.newaxis, ..., np.newaxis]
         yield (raw_label, raw_input, i)
 
 
 def load_data():
-    data = dict()
-    data["train"] = lambda: load(config.data_path + "/train")
-    data["valid"] = lambda: load(config.data_path + "/valid")
+    data = {}
+    data["train"] = lambda: load(os.path.join(config.data_path, "train"))
+    data["valid"] = lambda: load(os.path.join(config.data_path, "valid"))
+    data["test"] = lambda: load(os.path.join(config.data_path, "test"))
     return data
